a3ef52ddfc0481d9ebbc925ee4ecbf231fb55fe6
Anubama <[anubama2006@gmail.com]>
2025-12-22 11:13:14 +0530

    SideMenu Created

--- frontend/vite-project/src/pages/Auth/Signup.jsx
+++ frontend/vite-project/src/pages/Auth/Signup.jsx
@@ -1,9 +1,13 @@
-import React, {useState} from 'react'
+import React, { useContext, useState} from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout';
 import {useNavigate, Link} from "react-router-dom";
-import Input from '../../components/layouts/Inputs/Input';
-import ProfilePhotoSelector from '../../components/layouts/Inputs/ProfilePhotoSelector';
+import Input from '../../components/Inputs/Input';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 import { validateEmail } from '../../utils/helper';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/UserContext';
+import uploadImage from '../../utils/uploadImage';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Signup = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +16,8 @@ const Signup = () => {
   const [password, setPassword] = useState("");
 
   const [error, setError] = useState(null);
+
+  const { updateUser } = useContext(UserContext);
 
   const navigate = useNavigate();
 
@@ -37,13 +43,44 @@ const Signup = () => {
     }
 
     setError("");
+
+    //SignUp API Call
+    try {
+
+      //Upload image if present 
+      if(profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl
+      });
+
+      const { token, user } = response.data;
+
+      if ( token ) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch ( error ){
+      if(error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError(" Something went wrong. Please try again.");
+      }
+    }
   }
 
   return (
     <AuthLayout>
-      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+      <div className='lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
         <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        <p className='text-xs text-slate-700 mt-1.25 mb-6'>
           Join us today by entering your details below.
         </p>
 
--- frontend/vite-project/src/pages/Dashboard/Home.jsx
+++ frontend/vite-project/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { API_PATHS } from '../../utils/apiPaths';
+import axiosInstance from '../../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  useUserAuth();   
 
-export default Home
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if(loading) return;
+
+    setLoading(true);
+
+    try{
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if(response.data) {
+        setDashboardData(response.data);
+      }
+    } catch(error) {
+      console.log("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <DashboardLayout activeMenu = "Dashboard">
+      <div className='my-5 mx-auto'></div>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
